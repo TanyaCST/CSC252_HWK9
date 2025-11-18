6654551839feec79195b4701ccdcbf1a970eca02
Emilyyyy1021 <157932577+Emilyyyy1021@users.noreply.github.com>
2025-11-17 23:55:06 -0500

    dictionary!

--- hwk9.py
+++ hwk9.py
@@ -2,12 +2,24 @@
 # Peers:      Maggie Hollis (CSC TA)
 # References: Greedy Algotithm Lecture Note 
 #             https://docs.python.org/3/library/csv.html
+#             https://www.geeksforgeeks.org/python/load-csv-data-into-list-and-dictionary-using-python/
 import csv
-with open('color-US-states.csv') as US_map:
-    map_reader = csv.reader(US_map, delimiter=' ', quotechar='|')
-    for row in map_reader:
-        pass
 
+# with open('color-US-states.csv') as US_map:
+#     map_reader = csv.reader(US_map, delimiter=' ', quotechar='|')
+#     for row in map_reader:
+#         pass
+
+states: dict[str, str] = {}
+
+with open("color-US-states.csv", 'r') as f:
+    dict_reader = csv.DictReader(f)
+    for row in dict_reader:
+        key = row["STATE"]
+        value = row["NEIGHBORS"]
+        states[key] = value
+    
+print(states)
 
 #### First Greedy Strategy ####
 
