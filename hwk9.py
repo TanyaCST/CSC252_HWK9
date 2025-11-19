2623ccdbb114c66e90177143e24978dcf4f38e4a
Emilyyyy1021 <157932577+Emilyyyy1021@users.noreply.github.com>
2025-11-19 00:46:45 -0500

    worked!!!!!!!!

--- hwk9.py
+++ hwk9.py
@@ -10,16 +10,16 @@ import csv
 #     for row in map_reader:
 #         pass
 
-states: dict[str, str] = {}
+states: dict[str, list[str]] = {}
 
 with open("color-US-states.csv", 'r') as f:
     dict_reader = csv.DictReader(f)
     for row in dict_reader:
         key = row["STATE"]
-        value = row["NEIGHBORS"]
+        value = [n.strip() for n in row["NEIGHBORS"].split(',') if n.strip()] #没看到 先问一下chat 再改
         states[key] = value
     
-print(states)
+#print(states)
 
 
 #### First Greedy Strategy ####
@@ -27,9 +27,9 @@ print(states)
 
 #### Second Greedy Strategy ####
 # Emily: start w/ fewer neighbors
-states_sets = set(states)
+states_sets = set(states.keys())
 colored_states = {state: "" for state in states.keys()}
-colors = ["red", "orange", "yellow", "green", "blue", "inidigo", "violet"] #change it to simple array
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"] #change it to simple array
 
 
 # some thoughts
@@ -46,3 +46,32 @@ colors = ["red", "orange", "yellow", "green", "blue", "inidigo", "violet"] #chan
 #   no/else: color it with the first color in list colors, update colored_states
 
 # remove from states_sets
+
+while states_sets:
+
+    fewest_key: str|None = None
+    smallest_len = float('inf')
+
+    for key in states_sets:
+        num_neighbors = len(states[key])
+        if num_neighbors < smallest_len:
+            smallest_len = num_neighbors
+            fewest_key = key
+    
+    if fewest_key is not None:
+        if states[fewest_key] == []: 
+            colored_states[fewest_key] = colors[0]
+        else:
+            for color in colors:
+                conflict = False
+                for neighbor in states[fewest_key]:
+                    if colored_states.get(neighbor) == color:
+                        conflict = True
+                        break
+                if not conflict:
+                    colored_states[fewest_key] = color
+                    break
+
+        states_sets.remove(fewest_key)
+
+print(colored_states)
